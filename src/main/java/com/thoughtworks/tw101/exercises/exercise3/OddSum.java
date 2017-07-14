0f7a55a844fbaf6eae309a2443633d679c255c51
Goodness <godaramola@cpp.edu>
2017-07-14 16:00:55 -0600

    Exercise 3 complete

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,15 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+        int sum = 0;
+
+        if(start %2 == 0){
+            start++;
+        }
+        while(start < end){
+            sum+=start;
+            start += 2;
+        }
+        return sum;
     }
 }
